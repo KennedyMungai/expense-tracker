14295ad5657d574cc77bb4e01fda1318d39b7109
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-02 15:01:48 +0300

    Made use ofthe expenseDate

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -7,7 +7,7 @@ const ExpenseItem = () =>
 
     return (
         <div className='expense-item'>
-            <div> March 28th 2021</div>
+            <div>{expenseDate.toDateString()}</div>
             <div className="expense-item__description">
                 <h2>Car Insurance</h2>
                 <div className="expense-item__price">$294.67</div>
