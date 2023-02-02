3217684e7f9278d7fbf9c6654ee696b7af3d2dbd
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-02 15:03:56 +0300

    Defined and implemented the expenseAmount

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,13 +5,14 @@ const ExpenseItem = () =>
 {
     const expenseDate = new Date(2021, 2, 28)
     const expenseTitle = 'Car Insurance'
+    const expenseAmount = 294.67
 
     return (
         <div className='expense-item'>
             <div>{expenseDate.toDateString()}</div>
             <div className="expense-item__description">
                 <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">$294.67</div>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
     )
