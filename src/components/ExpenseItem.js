f3dbff7d0c9f796e66eb155348f58903fa5f94e6
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-02 15:04:54 +0300

    Made the date object convert to iso string

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -9,7 +9,7 @@ const ExpenseItem = () =>
 
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toDateString()}</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
                 <h2>{expenseTitle}</h2>
                 <div className="expense-item__price">${expenseAmount}</div>
