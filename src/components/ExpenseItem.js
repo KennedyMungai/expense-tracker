e30578c16b05458b22850ebee1efacd5b13ac66e
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-02 15:03:07 +0300

    Defined and implemented the expense title

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,12 +4,13 @@ import './ExpenseItem.css'
 const ExpenseItem = () =>
 {
     const expenseDate = new Date(2021, 2, 28)
+    const expenseTitle = 'Car Insurance'
 
     return (
         <div className='expense-item'>
             <div>{expenseDate.toDateString()}</div>
             <div className="expense-item__description">
-                <h2>Car Insurance</h2>
+                <h2>{expenseTitle}</h2>
                 <div className="expense-item__price">$294.67</div>
             </div>
         </div>
