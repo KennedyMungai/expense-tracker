1e0d34aa3545bfc69debeb0f26fbbd7b6accf836
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-02 15:44:39 +0300

    Implementing some basic formatting for the date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -10,9 +10,9 @@ const ExpenseItem = (props) =>
     return (
         <div className='expense-item'>
             <div>
-                <div className="">Month</div>
-                <div className="">Year</div>
-                <div className="">Day</div>
+                <div>{props.date.toLocaleString('en-us', { month: 'long' })}</div>
+                <div>{props.date.getFullYear()}</div>
+                <div>{props.date.toLocaleString('en-us', { day: '2-digit' })}</div>
             </div>
             <div className="expense-item__description">
                 <h2>{props.title}</h2>
