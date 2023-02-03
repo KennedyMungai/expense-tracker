35244afc86cb660fbb05df720302151f2ade93ad
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-03 09:08:15 +0300

    Made a few changes

--- src/components/ExpenseDate.js
+++ src/components/ExpenseDate.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-const ExpenseDate = () =>
+const ExpenseDate = (props) =>
 {
+    const month = props.date.toLocaleString('en-us', { month: 'long' })
+    const year = props.date.getFullYear()
+    const day = props.date.toLocaleString('en-us', { day: '2-digit' })
+
     return (
-        <div>ExpenseDate</div>
+        <div>
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
+        </div>
     )
 }
 
