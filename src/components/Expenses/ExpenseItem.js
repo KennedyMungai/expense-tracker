f3d199b2d62ca900d534228388222298ae091a9f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-03 14:33:55 +0300

    Added an onClick event to the button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -12,7 +12,7 @@ const ExpenseItem = (props) =>
                 <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button>Change Title</button>
+            <button onClick={() => console.log("Eye of the tiger")}>Change Title</button>
         </Card>
     )
 }
