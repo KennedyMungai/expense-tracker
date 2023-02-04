87739b2e904b4e2da56859a338442c900281fff1
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-04 12:03:51 +0300

    Changed the date initialization

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -55,7 +55,7 @@ const ExpenseForm = () =>
         const expenseData = {
             title: enteredTitle,
             amount: enteredAmount,
-            date: enteredDate
+            date: new Date(enteredDate)
         }
 
         // setEnteredAmount(0)
