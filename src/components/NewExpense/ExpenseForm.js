36e0cc16397757b2eaefced6a50595224bb76e22
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-04 11:51:58 +0300

    Reverted back to the old code

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -3,43 +3,49 @@ import './ExpenseForm.css'
 
 const ExpenseForm = () =>
 {
-    // const [enteredTitle, setEnteredTitle] = useState('')
-    // const [enteredAmount, setEnteredAmount] = useState()
-    // const [enteredDate, setEnteredDate] = useState()
+    const [enteredTitle, setEnteredTitle] = useState('')
+    const [enteredAmount, setEnteredAmount] = useState()
+    const [enteredDate, setEnteredDate] = useState()
 
-    const [userInput, setUserInput] = useState({
-        enteredTitle: '',
-        enteredAmount: '',
-        enteredDate: ''
-    })
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle: '',
+    //     enteredAmount: '',
+    //     enteredDate: ''
+    // })
 
     const titleChangeHandler = (e) => 
     {
+        setEnteredTitle(e.target.value)
+
         // setUserInput({
         //     ...userInput,
         //     enteredTitle: e.target.value
         // })
 
-        setUserInput((prevState) =>
-        {
-            return { ...prevState, enteredTitle: e.target.value }
-        })
+        // setUserInput((prevState) =>
+        // {
+        //     return { ...prevState, enteredTitle: e.target.value }
+        // })
     }
 
     const amountChangeHandler = (e) => 
     {
-        setUserInput({
-            ...userInput,
-            enteredAmount: e.target.value
-        })
+        setEnteredAmount(e.target.value)
+
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount: e.target.value
+        // })
     }
 
     const dateChangeHandler = (e) => 
     {
-        setUserInput({
-            ...userInput,
-            enteredDate: e.target.value
-        })
+        setEnteredDate(e.target.value)
+
+        // setUserInput({
+        //     ...userInput,
+        //     enteredDate: e.target.value
+        // })
     }
 
     return (
