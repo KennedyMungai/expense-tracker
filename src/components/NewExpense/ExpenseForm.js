90cbef6cd8caa3d34f030d737dec2c96d7252ebe
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-04 11:07:26 +0300

    Added and styled the AMount form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -10,6 +10,14 @@ const ExpenseForm = () =>
                     <label htmlFor="new_expense__title">Title</label>
                     <input type="text" name="title" id="new_expense__title" />
                 </div>
+                <div className="new-expense__control">
+                    <label htmlFor="new_expense__amount">Amount</label>
+                    <input type="number" name="amount" id="new_expense__amount" min='0.01' step='0.01' />
+                </div>
+                <div className="new-expense__control">
+                    <label htmlFor="new_expense__title">Title</label>
+                    <input type="text" name="title" id="new_expense__title" />
+                </div>
             </div>
         </form>
     )
