ac44232ebf06466c3c52074491ff4cb09664961c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-04 11:08:51 +0300

    Added some more DOM elements to the form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -15,8 +15,8 @@ const ExpenseForm = () =>
                     <input type="number" name="amount" id="new_expense__amount" min='0.01' step='0.01' />
                 </div>
                 <div className="new-expense__control">
-                    <label htmlFor="new_expense__title">Title</label>
-                    <input type="text" name="title" id="new_expense__title" />
+                    <label htmlFor="new_expense__date">Date</label>
+                    <input type="date" name="date" id="new_expense__date" min='2023-01-01' max='2025-12-31' />
                 </div>
             </div>
         </form>
