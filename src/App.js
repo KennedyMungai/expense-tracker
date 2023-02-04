3a9f951c29ae72f5e3b4592267e6ca66bb756e2f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-04 11:13:27 +0300

    Made a few changes to the logic

--- src/App.js
+++ src/App.js
@@ -27,12 +27,14 @@ function App()
   ];
 
   return (
-    // <div className="App">
-    //   <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-    //   <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-    //   <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[1].date} />
-    // </div>
-    <NewExpense />
+    <>
+      <NewExpense />
+      <div className="App">
+        <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
+        <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
+        <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[1].date} />
+      </div>
+    </>
   );
 }
 
