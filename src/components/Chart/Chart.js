48946e97f8d8362b47c0582d61c0b0fbcd7c6660
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-06 14:53:46 +0300

    Added the label prop to the CharBar component:

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -6,7 +6,7 @@ const Chart = (props) =>
 {
     return (
         <div className='chart'>
-            {props.dataPoints.map((dataPoint) => <ChartBar value={dataPoint.value} maxValue={null} />)}
+            {props.dataPoints.map((dataPoint) => <ChartBar value={dataPoint.value} maxValue={null} label={dataPoint.label} />)}
         </div>
     )
 }
