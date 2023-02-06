55350a42b50cb4dd16101ebf296b322f005de1f0
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-06 14:46:39 +0300

    Formatter at work

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Chart.css'
 
-const Chart = () => {
-  return (
-    <div>Chart</div>
-  )
+const Chart = () =>
+{
+    return (
+        <div>Chart</div>
+    )
 }
 
 export default Chart
