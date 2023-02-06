2bc17c83e7d375d357caa31927b89321a4f3494d
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-06 15:04:26 +0300

    Defined the percentage logic for the barFillHeight

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -5,6 +5,11 @@ const ChartBar = (props) =>
 {
     let barFillHeight = '0%'
 
+    if (props.value > 0)
+    {
+        barFillHeight = Math.round((props.value / props.maxValue) * 100)
+    }
+
     return (
         <div className='chart-bar'>
             <div className="chart-bar__inner">
