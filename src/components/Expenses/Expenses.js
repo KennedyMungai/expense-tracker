000e46d1950887959343a1327713768326b6bb53
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-06 14:27:47 +0300

    Extracted some of the code to their own components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -20,19 +20,7 @@ const Expenses = (props) =>
     }
     )
 
-    let expensesContent = <p>No Expenses found</p>
 
-    if (filteredExpenses.length > 0)
-    {
-        expensesContent = filteredExpenses.map((expense) => (
-            <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date}
-            />
-        ))
-    }
 
     return (
         <div>
