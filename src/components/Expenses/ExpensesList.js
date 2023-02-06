a0f1008b84d8f320829bb33f3914bfc8e4ef79cf
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-02-06 14:27:25 +0300

    Pimped out the react component

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import ExpenseItem from './ExpenseItem'
 
 const ExpensesList = (props) =>
 {
-    return (
-        <div>ExpensesList</div>
-    )
+    let expensesContent = <p>No Expenses found</p>
+
+    if (props.items.length > 0)
+    {
+        expensesContent = props.items.map((expense) => (
+            <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />
+        ))
+    }
 }
 
 export default ExpensesList
